c4b848d64890b2f57be4b472115c4463c0c29b81
GeoVolunteer <pnjogubless@gmail.com>
2023-05-08 14:39:36 +0300

    adding the return method in every class

--- Geo/Geovolunteer/models.py
+++ Geo/Geovolunteer/models.py
@@ -8,15 +8,22 @@ class Location(models.Model):
     state = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
 
+    def _str_(self):
+        return self.name 
 class Admin(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
 
+    def _str_(self):
+        return self.name 
 class Volunteers(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
+    def _str_(self):
+        return self.name 
 
 class Events(models.Model):
     title = models.CharField(max_length=255)
@@ -26,4 +33,6 @@ class Events(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
     volunteers = models.ManyToManyField(Volunteers)
+    def _str_(self):
+        return self.name 
 # Create your models here.
