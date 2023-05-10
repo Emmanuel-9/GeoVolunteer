ea0e11e02a4ef35c99b6fab0f4034fc851e3a023
Emmanuel-9 <oregadaniel181@gmail.com>
2023-05-10 05:33:03 +0300

    Register the created models.

--- Geo/Geovolunteer/admin.py
+++ Geo/Geovolunteer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Location)
+admin.site.register(Volunteers)
+admin.site.register(Events)
